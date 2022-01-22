77cf5d6a08f2a212b1d32b90e2991fcc9c6b4f8e
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-01-22 12:54:08 +0900

    userにrequestのルーティングをネストさせる

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
+  root 'requests#index'
+  # get 'users/index'
+  # get 'users/show'
   devise_for :users
+  resources :users do
+    resources :requests
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
