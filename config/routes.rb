a986de99292119429e8bb74fa05123ded6b83409
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-01-22 12:55:19 +0900

    ルーティングのトップを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  root 'requests#index'
-  # get 'users/index'
-  # get 'users/show'
+  root 'users#index'
   devise_for :users
   resources :users do
     resources :requests
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
