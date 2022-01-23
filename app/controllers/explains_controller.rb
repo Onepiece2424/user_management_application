fede7e6424aecad547af5891931ddaa0d40359bc
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-01-23 15:50:21 +0900

    不要な部分の削除

--- app/controllers/explains_controller.rb
+++ app/controllers/explains_controller.rb
@@ -1,29 +1,23 @@
 class ExplainsController < ApplicationController
   before_action :set_explain, only: %i[ show edit update destroy ]
 
-  # GET /explains or /explains.json
   def index
-    # @explains = Explain.all
     @user = User.where(:id => params[:user_id]).first
     @explains = @user.explains.all
   end
 
-  # GET /explains/1 or /explains/1.json
   def show
 
   end
 
-  # GET /explains/new
   def new
     @explain = Explain.new
     @user = User.find(params[:user_id])
   end
 
-  # GET /explains/1/edit
   def edit
   end
 
-  # POST /explains or /explains.json
   def create
     @explain = Explain.new(explain_params)
 
@@ -38,7 +32,6 @@ class ExplainsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /explains/1 or /explains/1.json
   def update
     respond_to do |format|
       if @explain.update(explain_params)
@@ -51,7 +44,6 @@ class ExplainsController < ApplicationController
     end
   end
 
-  # DELETE /explains/1 or /explains/1.json
   def destroy
     @explain.destroy
 
@@ -62,12 +54,10 @@ class ExplainsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_explain
       @explain = Explain.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def explain_params
       params.require(:explain).permit(:title, :precedure1, :image1, :precedure2, :image2, :precedure3, :image3)
     end
