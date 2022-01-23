4f9098229aec629f3bc129f64b7fd161f3582517
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-01-24 06:02:05 +0900

    関連付けの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  # belongs_to :request
+  has_many :requests
   has_many :explains
 end
