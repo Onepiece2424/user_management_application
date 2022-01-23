0b420698dc94abe5dee40ff16078b7a7b076a5d9
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-01-23 10:23:39 +0900

    7つのアクションの変数の設定

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -6,19 +6,35 @@ class RequestsController < ApplicationController
 
   def new
     @request = Request.new
+    @user = User.find(params[:user_id])
   end
 
   def create
-
+    @request = Request.new(request_params)
+    if @request.save
+      redirect_to user_requests_path
+    end
   end
 
   def show
-    # @request = Request.find(params[:id])
+    @request = Request.find(params[:id])
+    @user = User.find(params[:user_id])
   end
 
   def edit
+    @request = Request.find(params[:id])
+    @user = User.find(params[:user_id])
   end
 
-  def delete
+  def destroy
+    @request = Request.find(params[:id])
+    @request.destroy
+
+    redirect_to user_request_path
+  end
+
+  private
+  def request_params
+    params.require(:request).permit(:requesting, :who, :whom, :deadline)
   end
 end
