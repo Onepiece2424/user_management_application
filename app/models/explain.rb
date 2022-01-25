394c9b38f150a1a90c87c9c054b9b87c07192a6a
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-01-26 06:35:14 +0900

    carrierwaveの追加、in¥mgカラムの追加、アップローダーの導入

--- app/models/explain.rb
+++ app/models/explain.rb
@@ -1,3 +1,4 @@
 class Explain < ApplicationRecord
   belongs_to :user, optional: true
+  mount_uploader :img, ImgUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_23_063858) do
+ActiveRecord::Schema.define(version: 2022_01_25_210122) do
 
   create_table "explains", force: :cascade do |t|
     t.string "title"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2022_01_23_063858) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.string "img"
   end
 
   create_table "requests", force: :cascade do |t|
