14bc61ab5fa243c7355a10add97eda3d21b9781b
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-02-05 10:11:52 +0900

    不要なメソッドの削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,15 +4,4 @@ class UsersController < ApplicationController
     @users = User.all
     @requests = Request.all
   end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-  end
-
-  def show
-    # @user = User.find(params[:id])
-  end
 end
